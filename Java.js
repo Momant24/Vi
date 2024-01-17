a27122a63c91308f8c96e86b483af7d15a1c0942
Momant24 <111640190+Momant24@users.noreply.github.com>
2024-01-17 09:47:12 +0100

    Mer

--- Java.js
+++ Java.js
@@ -1,1 +1,17 @@
-var morsomt
+document.getElementById("fargeEndreKnapp").addEventListener("click", function() {
+    document.body.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+});
+
+document.getElementById("tekstEndreKnapp").addEventListener("click", function() {
+    document.getElementById("fargeEndreKnapp").textContent = "Farge Endret!";
+});
+
+let poeng = 0;
+document.getElementById("spillKnapp").addEventListener("click", function() {
+    document.getElementById("spillContainer").style.display = "block";
+});
+
+document.getElementById("poengKnapp").addEventListener("click", function() {
+    poeng++;
+    document.getElementById("poeng").textContent = poeng;
+});
